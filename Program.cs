9525db2c09169c04fa3dab9be476c7000494af72
marka <marka@MARKA>
2022-12-19 18:37:23 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace _S1_P1
+{
+    internal class Program
+    {
+        static void Main(string[]args)
+        {
+
+            Console.Write(Convert.ToString(System.DateTime.Now.Hour));
+            Console.Write(":");
+            Console.Write(Convert.ToString(System.DateTime.Now.Minute));
+            Console.Write(":");
+            Console.WriteLine(Convert.ToString(System.DateTime.Now.Second));
+            Console.Write(Convert.ToString(System.DateTime.Now.Hour, 2));
+            Console.Write(":");
+            Console.Write(Convert.ToString(System.DateTime.Now.Minute, 2));
+            Console.Write(":");
+            Console.WriteLine(Convert.ToString(System.DateTime.Now.Second, 2));
+            Console.ReadKey();
+        }
+    }
+    
+}
